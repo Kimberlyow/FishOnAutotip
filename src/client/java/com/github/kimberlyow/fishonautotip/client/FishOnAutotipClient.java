97c4f9eb3a270dbb44b92dc5ce1bd8a08458efc5
Kimberlyow <119278603+Kimberlyow@users.noreply.github.com>
2026-09-11 08:34:14 +0200

    initial commit, fabric template

---
+++ src/client/java/com/github/kimberlyow/fishonautotip/client/FishOnAutotipClient.java
@@ -0,0 +1,10 @@
+package com.github.kimberlyow.fishonautotip.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishOnAutotipClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
